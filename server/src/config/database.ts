ee08e5b94ad367ec8854483b4cd29a45a7f1d6a8
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-06-04 15:25:03 +0300

    Add production database on azure

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,11 +1,26 @@
 import { Sequelize } from "sequelize";
 
-const { DB_NAME } = process.env ?? "postgres";
-const { DB_USER } = process.env ?? "postgres";
-const { DB_PASSWORD } = process.env;
-const { DB_HOST } = process.env ?? "127.0.0.1";
-const { DB_PORT } = process.env ?? "5666";
+const { DEV_DB_NAME } = process.env;
+const { DEV_DB_USER } = process.env;
+const { DEV_DB_PASSWORD } = process.env;
+const { DEV_DB_HOST } = process.env;
+const { DEV_DB_PORT } = process.env;
 
-const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const { PROD_DB_NAME } = process.env;
+const { PROD_DB_USER } = process.env;
+const { PROD_DB_PASSWORD } = process.env;
+const { PROD_DB_HOST } = process.env;
+const { PROD_DB_PORT } = process.env;
+
+const DEV_URI = `postgres://${DEV_DB_USER}:${DEV_DB_PASSWORD}@${DEV_DB_HOST}:${DEV_DB_PORT}/${DEV_DB_NAME}`;
+const PROD_URI = `postgres://${PROD_DB_USER}:${PROD_DB_PASSWORD}@${PROD_DB_HOST}:${PROD_DB_PORT}/${PROD_DB_NAME}?ssl=true`;
+
+let URI = null;
+
+if (process.env.NODE_ENV === "production") {
+  URI = PROD_URI;
+} else {
+  URI = DEV_URI;
+}
 
 export const db = new Sequelize(URI);
