3f5e3bd2fcf467db74e05585050f1cf55de48dfe
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-06-24 23:41:23 +0300

    Design Logout component

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -8,25 +8,34 @@ const handleSignInClick = () => {
 
 export const Login = () => {
   return (
-    <Flex width="full" align="center" justifyContent="center">
-      <Box p={8} maxWidth="500px" borderRadius={8}>
-        <Box textAlign="center">
-          <Heading>Log in to PrivateVoid</Heading>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Flex width="full" align="center" justifyContent="center">
+        <Box p={8} maxWidth="500px" borderRadius={8}>
+          <Box textAlign="center">
+            <Heading>Log in to PrivateVoid</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <Button
+              onClick={handleSignInClick}
+              colorScheme="purple"
+              size="lg"
+              leftIcon={<FaGithub />}
+              width="full"
+              mt={4}
+              rounded={'full'}
+            >
+              GitHub
+            </Button>
+          </Box>
         </Box>
-        <Box my={4} textAlign="left">
-          <Button
-            onClick={handleSignInClick}
-            colorScheme="purple"
-            size="lg"
-            leftIcon={<FaGithub />}
-            width="full"
-            mt={4}
-            rounded={'full'}
-          >
-            GitHub
-          </Button>
-        </Box>
-      </Box>
-    </Flex>
+      </Flex>
+    </div>
   );
 };
--- client/src/components/logout/Logout.js
+++ client/src/components/logout/Logout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
+import { Button, Box, Heading, Text, Stack, Center } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const handleLogoutClick = () => {
   window.open('http://localhost:8080/auth/logout', '_self');
@@ -7,13 +8,45 @@ const handleLogoutClick = () => {
 
 export const Logout = () => {
   return (
-    <Button
-      onClick={handleLogoutClick}
-      colorScheme="purple"
-      variant="ghost"
-      size="md"
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
     >
-      Log out
-    </Button>
+      <Box
+        borderRadius="2xl"
+        borderWidth="1px"
+        alignItems="center"
+        padding="50px"
+      >
+        <Heading textAlign="center" paddingBottom="10px">
+          Log out of PrivateVoid?
+        </Heading>
+        <Text colorScheme={'gray'} maxW={'sm'} textAlign="center">
+          You can always log back in at any time. If you just want to switch
+          accounts, you can do that by adding an existing account.
+        </Text>
+        <Center>
+          <Stack spacing={6} direction={'row'} paddingTop="20px">
+            <Link to="/home">
+              <Button rounded={'full'} px={6}>
+                Cancel
+              </Button>
+            </Link>
+            <Button
+              rounded={'full'}
+              px={6}
+              colorScheme={'purple'}
+              onClick={handleLogoutClick}
+            >
+              Log out
+            </Button>
+          </Stack>
+        </Center>
+      </Box>
+    </div>
   );
 };
