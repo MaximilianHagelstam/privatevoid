30168cd23cec2000a2e0e30df23d52c4d23955c6
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-06-24 12:14:54 +0300

    Add login check to ladning page

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Hero } from './Hero';
 import { LandingLayout } from './LandingLayout';
 
 export const Landing = () => {
+  let history = useHistory();
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/current-user', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': true,
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        console.log('You are logged in');
+        history.push('/home');
+      }
+    });
+  }, [history]);
+
   return (
     <LandingLayout>
       <Hero
