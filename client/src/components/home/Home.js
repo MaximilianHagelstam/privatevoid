9dbaa4a199a74a3729685896f83d446888ca1540
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-06-25 08:59:05 +0300

    Plan out home page layout with grid

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@chakra-ui/react';
+import { Button, Box, HStack } from '@chakra-ui/react';
 
 import { ToggleTheme } from './sidebar/ToggleTheme';
+// <div>
+//   <h1>Welcome to PrivateVoid</h1>
+//   <Link to="/logout">
+//     <Button colorScheme="purple">Logout</Button>
+//   </Link>
+//   <ToggleTheme />
+// </div>
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Welcome to PrivateVoid</h1>
-      <Link to="/logout">
-        <Button colorScheme="purple">Logout</Button>
-      </Link>
-      <ToggleTheme />
-    </div>
+    <HStack spacing="24px" paddingLeft="96px" paddingRight="96px">
+      <Box
+        w="25%"
+        h="750"
+        borderRadius="3xl"
+        borderWidth="1px"
+        alignItems="center"
+        padding="24px"
+      >
+        Sidebar
+      </Box>
+      <Box
+        w="50%"
+        h="750"
+        borderRadius="3xl"
+        borderWidth="1px"
+        alignItems="center"
+        padding="24px"
+      >
+        Feed
+      </Box>
+      <Box
+        w="25%"
+        h="750"
+        borderRadius="3xl"
+        borderWidth="1px"
+        alignItems="center"
+        padding="24px"
+      >
+        Discover
+      </Box>
+    </HStack>
   );
 };
