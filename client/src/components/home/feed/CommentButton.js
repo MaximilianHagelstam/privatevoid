28e9a50607216d39b1a0a926497772b837b62a3a
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-06-28 14:36:29 +0300

    Add create post frontend

--- client/src/components/home/feed/CommentButton.js
+++ client/src/components/home/feed/CommentButton.js
@@ -1,14 +1,82 @@
-import { IconButton } from '@chakra-ui/react';
+import {
+  IconButton,
+  Modal,
+  Button,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  Textarea,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { FiMessageCircle } from 'react-icons/fi';
+import React, { useState } from 'react';
+
+import { createComment } from '../../../util/api';
 
 export const CommentButton = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [comment, setComment] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createComment({ body: comment, postId: 1 });
+    setComment('');
+  };
+
+  const onChange = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
-    <IconButton
-      variant="ghost"
-      rounded={'full'}
-      colorScheme="purple"
-      aria-label="Comment"
-      icon={<FiMessageCircle />}
-    />
+    <div>
+      <IconButton
+        variant="ghost"
+        rounded={'full'}
+        colorScheme="purple"
+        aria-label="Comment"
+        onClick={onOpen}
+        icon={<FiMessageCircle />}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={onSubmit}>
+            <ModalHeader>Comment</ModalHeader>
+            <ModalCloseButton rounded={'full'} colorScheme="purple" />
+            <ModalBody pb={6}>
+              <FormControl>
+                <Textarea
+                  placeholder="Write your comment"
+                  size="lg"
+                  resize="none"
+                  isRequired={true}
+                  type="text"
+                  name="comment"
+                  value={comment}
+                  onChange={onChange}
+                />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="purple"
+                mr={3}
+                type="submit"
+                rounded={'full'}
+                onClick={onClose}
+              >
+                Comment
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </div>
   );
 };
