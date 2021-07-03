c854efec88e0dc782e96f210cda4a1246c14cd23
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-03 20:53:05 +0300

    Make settings placeholders current values

--- client/src/components/settings/index.js
+++ client/src/components/settings/index.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 import './Settings.css';
 
-import {} from '../../util/api';
+import { fetchUser } from '../../util/api';
 
 export const Settings = () => {
   const [displayName, setDisplayName] = useState('');
   const [bio, setBio] = useState('');
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchUser()
+      .then((res) => {
+        return res.json();
+      })
+      .then((resJson) => {
+        if (resJson) {
+          setUser(resJson);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   const onSubmit = (e) => {
     // e.preventDefault();
@@ -25,7 +41,7 @@ export const Settings = () => {
         <FormLabel>Display Name</FormLabel>
         <Input
           type="text"
-          placeholder="Peter"
+          placeholder={user.display_name}
           size="lg"
           onChange={(e) => setDisplayName(e.target.value)}
           rounded={'full'}
@@ -35,7 +51,7 @@ export const Settings = () => {
         <FormLabel>Bio</FormLabel>
         <Input
           type="text"
-          placeholder="Yo Yo"
+          placeholder={user.bio}
           size="lg"
           onChange={(e) => setBio(e.target.value)}
           rounded={'full'}
