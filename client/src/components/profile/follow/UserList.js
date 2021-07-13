1fc4146deb7932f8641f46bd4c74a593c8061136
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-13 23:26:53 +0300

    Add user link in follow modals

--- client/src/components/profile/follow/UserList.js
+++ client/src/components/profile/follow/UserList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Flex, Avatar, Heading, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export const UserList = ({ avatar, displayName, username }) => {
   return (
-    <div style={{ alignItems: 'center', display: 'flex' }}>
-      <Avatar size="md" src={avatar} />
-      <Flex flexDir="column" padding="10px">
-        <Heading as="h3" size="sm">
-          {displayName}
-        </Heading>
-        <Text color="gray">@{username}</Text>
-      </Flex>
-    </div>
+    <Link to={`/user/${username}`}>
+      <div style={{ alignItems: 'center', display: 'flex' }}>
+        <Avatar size="md" src={avatar} />
+        <Flex flexDir="column" padding="10px">
+          <Heading as="h3" size="sm">
+            {displayName}
+          </Heading>
+          <Text color="gray">@{username}</Text>
+        </Flex>
+      </div>
+    </Link>
   );
 };
