e682891d2e50d80a89c839a72fbced7d7e6bdb5e
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-07-16 23:20:21 +0300

    Add user profile design to explore

--- client/src/components/common/layout/explore/index.js
+++ client/src/components/common/layout/explore/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Flex, Avatar, Text } from '@chakra-ui/react';
 
 import './Explore.css';
+import './User.css';
 
 export const Explore = () => {
   return (
     <div className="explore">
       <div className="widget">
-        <Heading size="md">Most followed</Heading>Yo Yo
+        <Heading size="md" paddingBottom="10px">
+          Most followed
+        </Heading>
+        <div className="user">
+          <Avatar
+            size="md"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEWAgICQdD0xAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII="
+          />
+          <Flex flexDir="column" padding="10px">
+            <Heading as="h3" size="sm">
+              James
+            </Heading>
+            <Text color="gray">@JamesDeez</Text>
+          </Flex>
+        </div>
       </div>
     </div>
   );
